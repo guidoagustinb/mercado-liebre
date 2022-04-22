666692c727e79b66856a949ac24abbbb0dbf3e66
guidoagustinb <guidovp3@gmail.com>
2022-04-21 23:01:31 -0300

    firstc

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require ("express")
+const app = express ()
+const path = require ("path")
+const publicPath = path.resolve(__dirname, "./public")
+
+app.use (express.static(publicPath));
+
+app.listen (process.env.PORT || 3000, () => (
+    console.log ("server anda")
+))
+
+// app.listen (3032, () => (
+//     console.log ("server anda")
+// ))
+
+app.get ("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get ("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get ("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
